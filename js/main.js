0ee7518fecb20083168f1b7c002fa8a2d2123bea
Davide <dadavidenapolitano@gmail.com>
2021-07-22 16:54:43 +0200

    Base struttura abbozzata

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Creiamo un finto biglietto del treno con:
+// Nome passeggero
+// Codice treno (numero casuale tra 90000 e 100000 escluso)
+// Numero carrozza
+// Prezzo calcolato
+// Categoria selezionata dall'utente
+// Aggiungiamo una piccola animazione al click su "Crea" e "Annulla", se clicchiamo su annulla dobbiamo ripulire il form.
+// BONUS: predisporre l'interfaccia grafica responsiva :faccia_leggermente_sorridente:
+// Buon lavoro e buon... viaggio :occhiolino:
+
